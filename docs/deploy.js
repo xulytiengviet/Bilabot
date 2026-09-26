f2dc300d2d1a29fe8b88aa90db49dd4fbcd35d36
Long Ngo <kimphatngogia@gmail.com>
2026-09-26 07:53:09 +0700

    config: set live Cloudflare Pages gateway bilabot-web.pages.dev as default

--- docs/deploy.js
+++ docs/deploy.js
@@ -1,4 +1,4 @@
 /** Set this public URL after the Cloudflare Pages application is live.
  * GitHub Actions injects repository variable CLOUDFLARE_PAGES_URL.
  */
-window.BILABOT_DEPLOY_URL = '';
+window.BILABOT_DEPLOY_URL = 'https://bilabot-web.pages.dev';
